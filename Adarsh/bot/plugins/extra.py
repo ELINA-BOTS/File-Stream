118714d61ae9a9ef3c65da923f909d4af3347656
Zishan khan <142872501+Jisshubot@users.noreply.github.com>
2023-11-20 13:41:47 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,7 +17,7 @@ async def master(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Coded By [IT'S LUFFY](https://t.me/god_luffy_ati)",
+                    text="I am Coded By [ZISHAN](https://t.me/IM_JISSHU)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("FOLLOW ME", url=f"https://youtube.com/@elitecraft_studios")
+                                InlineKeyboardButton("FOLLOW ME", url=f"https://youtube.com/@JISSHU-BOTS")
                             ]
                         ]
                     ),
